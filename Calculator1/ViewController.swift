1c11decfd227d2bfe55f638106a7b3def40c4f05
Pavel <pavel.m.3107@gmail.com>
2019-12-09 12:52:44 +0200

    Initial Commit

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Pavel Ronaldo on 12/9/19.
+//  Copyright © 2019 Pavel Ronaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
